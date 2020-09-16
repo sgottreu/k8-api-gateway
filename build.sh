af7ff38eb2420f158aa0b75b932a0a284b03d63d
Scott Gottreu <sgottreu@gmail.com>
2020-09-16 16:27:16 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 docker build -t python -f python.Dockerfile .
-docker tag python reg.alta3.com/3245-gottreu-python-api
-docker push reg.alta3.com/3245-gottreu-python-api
+docker tag python <py image>
+docker push <py image>
+docker tag python <php image>
+docker push <pphp image>
 kubectl delete -f ~/websvc.yaml 
 kubectl apply -f ~/websvc.yaml
 kubectl port-forward api-svc 5000:5000 --address=0.0.0.0
